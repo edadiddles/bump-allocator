4690d8363e0979e74f18da0066a621a2fece4ad1
Edward Ruzek <edadiddles@hotmail.com>
2025-11-21 09:46:47 -0500

    adding init interface for zig allocator. Starting from definition of zig's FixedBufferAllocator

--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,21 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+const BumpAllocator = @This();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try stdout.flush(); // Don't forget to flush!
+pub fn allocator(self: *BumpAllocator) std.mem.Allocator {
+    return .{
+        .ptr = self,
+        .vtable = &.{
+            .alloc = alloc,
+            .resize = resize,
+            .remap = remap,
+            .free = free,
+        },
+    };
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+fn alloc(_: BumpAllocator) void {}
+fn resize(_: BumpAllocator) void {}
+fn remap(_: BumpAllocator) void {}
+fn free(_: BumpAllocator) void {}
