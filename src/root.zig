1e638f0e7c8d07b0abac02d195bc53c751eb24c2
Edward Ruzek <edadiddles@hotmail.com>
2025-11-22 12:27:38 -0500

    implemented alloc and init functions. setup resize, remap, and free to compile successfully but have noop functionality

--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,20 @@ const std = @import("std");
 
 const BumpAllocator = @This();
 
+const AllocationError = error{ OutOfMemory };
+
+buffer: []u8,
+offset: usize,
+
+
+pub fn init(buffer: []u8) BumpAllocator {
+    std.debug.print("buffer size: {}\n", .{ buffer.len });
+    return .{
+        .buffer = buffer,
+        .offset = 0,
+    };
+}
+
 pub fn allocator(self: *BumpAllocator) std.mem.Allocator {
     return .{
         .ptr = self,
@@ -15,7 +29,59 @@ pub fn allocator(self: *BumpAllocator) std.mem.Allocator {
     };
 }
 
-fn alloc(_: BumpAllocator) void {}
-fn resize(_: BumpAllocator) void {}
-fn remap(_: BumpAllocator) void {}
-fn free(_: BumpAllocator) void {}
+fn alloc(ctx: *anyopaque, n: usize, alignment: std.mem.Alignment, ra: usize) ?[*]u8 {
+    const self: *BumpAllocator = @ptrCast(@alignCast(ctx));
+    _ = ra;
+
+    const mem_align = alignment.toByteUnits();
+    const adj_size = std.mem.alignPointerOffset(self.buffer.ptr + self.offset, mem_align) orelse return null;
+    const adj_offset = self.offset + adj_size;
+    const new_offset = adj_offset + n;
+    if (new_offset >= self.buffer.len) return null;
+    self.offset = new_offset;
+    
+    return self.buffer.ptr + adj_offset;
+}
+
+fn resize(ctx: *anyopaque, buf: []u8, alignment: std.mem.Alignment, new_size: usize, return_address: usize) bool {
+    const self: *BumpAllocator = @ptrCast(@alignCast(ctx));
+    _ = self;
+    _ = buf;
+    _ = alignment;
+    _ = new_size;
+    _ = return_address;
+
+    return false;
+}
+
+fn remap(ctx: *anyopaque, memory: []u8, alignment: std.mem.Alignment, new_len: usize, return_address: usize) ?[*]u8 {
+    const self: *BumpAllocator = @ptrCast(@alignCast(ctx));
+    _ = self;
+    _ = memory;
+    _ = alignment;
+    _ = new_len;
+    _ = return_address;
+
+    return null;
+}
+
+fn free(ctx: *anyopaque, buf: []u8, alignment: std.mem.Alignment, return_address: usize) void {
+    const self: *BumpAllocator = @ptrCast(@alignCast(ctx));
+    _ = self;
+    _ = buf;
+    _ = alignment;
+    _ = return_address;
+}
+
+var test_fixed_buffer_allocator_memory: [800000 * @sizeOf(u64)]u8 = undefined;
+var test_small_fixed_buffer_allocator_memory: [8 * @sizeOf(u64)]u8 align(@alignOf(u64)) = undefined;
+test "alloc" {
+    var bump_allocator = BumpAllocator.init(test_fixed_buffer_allocator_memory[0..]);
+    const a = bump_allocator.allocator();
+
+    for(0..100) |i| {
+        std.debug.print("---- {} ----\n", .{ i });
+        _ = try a.alloc(u8, 8);
+    }
+        
+}
